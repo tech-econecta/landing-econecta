eb594744b07f794c981a83dc0685d3ec6db2b98a
Your Name <darioflores170@gmail.com>
2025-10-12 22:31:11 -0400

    ajustes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  ...{
+  {
     rules: {
       "react/no-unescaped-entities": "off",
       "@next/next/no-img-element": "off",
